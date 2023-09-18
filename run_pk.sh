4f14e55271a0a5546d1ff69842ccd5fc29c674ab
Nicol?s Schiappacasse Vega <nicolaschiappacasse@gmail.com>
2023-09-17 21:36:29 -0300

    Add script for quick run

---
+++ run_pk.sh
@@ -0,0 +1,40 @@
+
+#! /bin/bash
+
+REPO_DIR="/home/zima/Documents/Outside_Life/Salud/TDAH/Simulation/flask_web/"
+CLI_EXE=$REPO_DIR/pk/cli.py
+VENV_DIR=$REPO_DIR/.venv
+
+# LDX PK PARAMETERS
+HL="10"
+TMAX="4.6"
+DOSE="1 1 1 1"
+OFFSETS="0 19 26 98"
+DURATION="300"
+
+
+EXE="python ${CLI_EXE} --hl ${HL} --tmax ${TMAX} --dose ${DOSE}  --offsets ${OFFSETS} --duration ${DURATION}"
+
+run_exe(){
+    echo "Running simulation.."
+    # Run formatter  
+    $EXE
+}
+
+# Check if the Python environment is activated
+if [[ "$VIRTUAL_ENV" == "" ]]; then
+    if [ -d "$VENV_DIR" ]; then
+        . $VENV_DIR/bin/activate
+        echo "Activated virtual environment.."
+        run_exe
+
+    else 
+        echo "Virtual environment not found."
+        exit 1
+    fi
+
+else
+    echo "Virtual environment already activated."
+fi
+
+ 
