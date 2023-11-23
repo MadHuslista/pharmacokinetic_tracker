55396b86ea409ab2c2be6d76acc6c1740e67a3a6
Nico_Schiappacasse <nicolaschiappacasse@gmail.com>
2023-11-23 19:28:16 -0300

    Refactor: Move arg parser from main to CLI

--- pk/main.py
+++ pk/main.py
@@ -1,11 +1,13 @@
 """Calculates and plots drug concentration over time."""
 
-import argparse
 import typing as tp
 
 import numpy as np
 
-from cli import cli_message
+from cli import (
+    arg_parser,
+    cli_message,
+)
 from config import START_TIME
 from graph import make_graph
 from time_tools import parse_input_time
@@ -31,6 +33,8 @@ def run_estimation(
     """Wrapper to calculate drug concentration over time."""
     step = 1/60
 
+    # TODO: Move to a dedicated module apart from pk.py
+
     drug = pk.Drug(half_life, time_to_max)
     num = round(estimation_duration / step + 1)
     x_hours = np.arange(num) * step
@@ -45,28 +49,8 @@ def run_estimation(
 
 def main():
     """The main function."""
-    ap = argparse.ArgumentParser(description=__doc__,
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    ap.add_argument('--hl', type=float, required=True, metavar='HOURS',
-                    help='the drug\'s elimination half-life, in hours')
-    ap.add_argument('--tmax', type=float, required=True, metavar='HOURS',
-                    help='the drug\'s time to maximum concentration, in hours')
-    ap.add_argument('--duration', type=float, default=24, metavar='HOURS',
-                    help='the duration, in hours, to simulate concentrations for')
-    ap.add_argument('--doses', type=float, nargs='+', default=[1], metavar='DOSE',
-                    help='the magnitudes of each dose (units are arbitrary)')
-    ap.add_argument('--offsets', type=float, nargs='+', default=[0], metavar='OFFSET',
-                    help='the time, in hours, that each dose is given at')
-    ap.add_argument('--output', default='output.png', metavar='FILE',
-                    help='the output image filename')
-    ap.add_argument('--output-size', type=int, nargs=2, default=[1920, 1280], metavar=('W', 'H'),
-                    help='the output width and height in pixels')
-    ap.add_argument('--dpi', type=float, default=160, help='the output dots per inch (dpi)')
-    ap.add_argument('-g', '--graph', action='store_true', help='show the graph')
 
-    ap.add_argument('-p','--parsetime', nargs='+', type=str, help='parse a time string and exit. Accepted formats: YYYY-MM-DD HH:MM, YY-MM-DD HH:MM, MM-DD HH:MM, DD HH:MM, HH:MM')
-
-    args = ap.parse_args()
+    args = arg_parser()
 
     if args.parsetime:
         parse_input_time(args.parsetime)
